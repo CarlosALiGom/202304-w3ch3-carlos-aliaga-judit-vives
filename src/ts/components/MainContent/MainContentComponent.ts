ee3fb43262454eb904efa696473d51fa258b3061
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-19 22:32:48 +0200

    Create mainContentCompent and implement on index.ts

---
+++ src/ts/components/MainContent/MainContentComponent.ts
@@ -0,0 +1,20 @@
+import type { ComponentStructure } from "../../types/types";
+import Component from "../Component/Component";
+
+class MainContentComponent extends Component implements ComponentStructure {
+  constructor(parentElement: Element) {
+    super(parentElement, "main", "main-content");
+
+    this.renderHtml();
+  }
+
+  renderHtml() {
+    this.element.innerHTML = `<h2 class="main-content__title">Series list</h2>
+      <section class="list">
+        <h3 class="list__title">Pending series</h3>
+        <span class="list__info">You have 4 series pending to watch</span>
+      </section>`;
+  }
+}
+
+export default MainContentComponent;
