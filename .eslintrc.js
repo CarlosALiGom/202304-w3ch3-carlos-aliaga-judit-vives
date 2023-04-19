9b81f4e6651bacdadb4e05aa31eec59d6163f468
Judit Vives <jvivesramon@gmail.com>
2023-04-19 19:17:18 +0200

    Install eslint, rules for eslint and ts

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: [["xo-typescript", "prettier"]],
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+        "no-used-vars": "error",
+        "no-new": off,
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "error",
+  },
+};
